53f86ba73077d333414617df461bad811da05bb0
Carles <lawer@users.noreply.github.com>
2024-11-01 22:42:22 +0100

    llevem imatges online

---
+++ baixa_imatges_incrustades.py
@@ -0,0 +1,47 @@
+import os
+import requests
+
+def listar_markdowns(directorio):
+    markdown_files = []
+    for root, dirs, files in os.walk(directorio):
+        for file in files:
+            if file.endswith('.md'):
+                markdown_files.append(os.path.join(root, file))
+    return markdown_files
+
+def download_images_and_substitute(fichero):
+    print(fichero)
+
+    with open(fichero, 'r') as file:
+        lines = file.readlines()
+    for i, line in enumerate(lines):
+        if line.startswith('!['):
+            print(line)
+            url = line.split('(')[1].split(')')[0]
+            if url.startswith('http'):
+                # Baixem la imatge amb requests
+
+                response = requests.get(url)
+                
+                # Guardem la imatge en un directori local
+                with open(f'apunts/images/{url.split("/")[-1]}', 'wb') as file:
+                    file.write(response.content)
+                
+                
+                # Modifiquem la línia perquè apunti a la imatge local
+                new_url = f'../../images/{url.split("/")[-1]}'
+                print(new_url)
+                lines[i] = line.replace(url, new_url)
+
+    with open(fichero, 'w') as file:
+        file.writelines(lines)    
+
+
+# Ejemplo de uso
+directorio = 'apunts'
+ficheros = listar_markdowns(directorio)
+
+for fichero in ficheros:
+    download_images_and_substitute(fichero)
+    
+print(ficheros)
